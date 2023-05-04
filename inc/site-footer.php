d78b955a88cb07d9f77512d6f5fae7ad960fb8d3
Kasutan <contact@kasutan.pro>
2023-05-04 17:06:05 +0200

    Contenu et style footer

--- inc/site-footer.php
+++ inc/site-footer.php
@@ -8,17 +8,18 @@
 add_action( 'tha_footer_top', 'kasutan_main_footer' );
 function kasutan_main_footer() {
 	
-
-	//TODO : ajouter tous droits réservés
+		$copyright="";
+		if(function_exists('get_field')) {
+			$copyright=wp_kses_post(get_field('eflex_copyright','option'));
+		}
+		if($copyright) printf('<span class="copy">%s</span>',$copyright);
 		//Liens
 		if( has_nav_menu( 'footer-liens')) {
-			echo '<div class="liens">';
 				
 				
 				if( has_nav_menu( 'footer-liens') ) {
 					wp_nav_menu( array( 'theme_location' => 'footer-liens', 'menu_id' => 'footer-liens', 'container_class' => 'nav-footer' ) );
 				}
-			echo '</div>';
 		}
 
 	echo '</div>';
