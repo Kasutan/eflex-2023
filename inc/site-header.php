c88048bd3d46c403555d9346a6a13a3d6cf1d915
Kasutan <contact@kasutan.pro>
2023-05-30 17:32:34 +0200

    Header toujours fixé à left 0 (sinon décalage en xxl et dézoom)

--- inc/site-header.php
+++ inc/site-header.php
@@ -2,6 +2,7 @@
 
 add_action('tha_header_top','kasutan_header_top');
 function kasutan_header_top() {
+	echo '<div class="header-container">';
 
 		printf('<div class="logo-header"><a href="%s" class="logo-link" title="Accueil">
 		%s</a></div>',
@@ -10,14 +11,15 @@ function kasutan_header_top() {
 		);
 
 
-	if( has_nav_menu( 'primary' ) ) {
-		echo '<nav id="site-navigation" class="nav-main" aria-label="menu principal">';
+		if( has_nav_menu( 'primary' ) ) {
+			echo '<nav id="site-navigation" class="nav-main" aria-label="menu principal">';
 
-		wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'container_class' => 'nav-primary' ) );
+			wp_nav_menu( array( 'theme_location' => 'primary', 'menu_id' => 'primary-menu', 'container_class' => 'nav-primary' ) );
 
-		echo '</nav>';
-	}
+			echo '</nav>';
+		}
 
-	do_action('wpml_add_language_selector');
+		do_action('wpml_add_language_selector');
 
+	echo '</div>';
 }
