7d54ae06a8dd340fcc2458bf43d8f5bd624be8c2
Kasutan <contact@kasutan.pro>
2024-06-16 18:33:23 +0200

    Volet avec CTAs liens Calendly

--- inc/site-header.php
+++ inc/site-header.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action('tha_header_top','kasutan_header_top');
+add_action('tha_header_top','kasutan_header_top',10);
 function kasutan_header_top() {
 	echo '<div class="header-container">';
 
@@ -21,5 +21,51 @@ function kasutan_header_top() {
 
 		do_action('wpml_add_language_selector');
 
+	echo '</div>';
+}
+
+
+/**
+* Afficher le volet avec les CTA Calendly dans l'en-tête
+*/
+add_action('tha_header_top','kasutan_affiche_volet_cta',5);
+function kasutan_affiche_volet_cta() {
+	if(!function_exists('get_field') ) {
+		return;
+	}
+	$cta_img_1=esc_attr(get_field('eflex_cta_image_1','option'));
+	$cta_img_2=esc_attr(get_field('eflex_cta_image_2','option'));
+	$cta_link_1=esc_url(get_field('eflex_cta_link_1','option'));
+	$cta_link_2=esc_url(get_field('eflex_cta_link_2','option'));
+	$cta_titre=wp_kses_post(get_field('eflex_cta_titre','option'));
+	$cta_texte=wp_kses_post(get_field('eflex_cta_texte','option'));
+
+	echo '<div class="volet-cta" id="volet-cta">';
+
+		echo '<div class="cadre">';
+			if($cta_titre) printf('<h2 class="titre-cta">%s</h2>',$cta_titre);
+
+			echo '<div class="cols-cta">';
+				if($cta_img_1 && $cta_link_1) {
+					printf('<a href="%s" class="link-cta" target="_blank" rel="noopener noreferrer">%s</a>',
+						$cta_link_1,
+						wp_get_attachment_image($cta_img_1, 'full', false, array("Alt"=>"Open link 1"))
+					);
+				}
+				if($cta_img_2 && $cta_link_2) {
+					printf('<a href="%s" class="link-cta" target="_blank" rel="noopener noreferrer">%s</a>',
+						$cta_link_2,
+						wp_get_attachment_image($cta_img_2, 'full', false, array("Alt"=>"Open link 1"))
+					);
+				}
+			echo '</div>';
+			if($cta_texte) printf('<p class="texte-cta">%s</p>',$cta_texte);
+
+			echo '<button id="fermer-cta" class="fermer"><span class="screen-reader-text">Close panel</span>
+				<svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 24 24" fill="none" stroke="#ffffff" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-x"><line x1="18" y1="6" x2="6" y2="18"/><line x1="6" y1="6" x2="18" y2="18"/></svg>
+			</button>';
+		echo '</div>';
+
+		
 	echo '</div>';
 }
--- js/main.js
+++ js/main.js
@@ -30,3 +30,42 @@ if ('IntersectionObserver' in window) {
 	//if Intersection Observer is not supported, add classes right away
 	jQuery('.home-logo').addClass('fade-in'); 
 }
+
+/*=================================================
+Fonctions jQuery
+=================================================*/
+(function($) {
+
+	$( document ).ready(function() {
+		/*********Afficher/masquer le volet avec les ctas **********/
+		var voletCTA=$('#volet-cta');
+		if($(voletCTA).length>0) {
+			var boutonCTA=$(".book-demo a");
+			//Le volet est fermé au chargement de la page
+			$(boutonCTA).attr('aria-expanded','false');
+
+			//Au clic sur un bouton
+			$(boutonCTA).click(function(){
+				if($(boutonCTA).attr('aria-expanded')=="false") {
+					$(voletCTA).fadeIn('slow');
+					$(voletCTA).css('display','flex');
+					$(boutonCTA).attr('aria-expanded','true');
+					$(this).addClass('last-focus');
+					$('#volet-cta a:first-of-type()').focus();
+					
+				} else {
+					$(voletCTA).fadeOut('slow');
+					$(boutonCTA).attr('aria-expanded','false');
+				}
+
+			});
+			$('#fermer-cta').click(function(){
+				$(voletCTA).fadeOut('slow');
+				$(boutonCTA).attr('aria-expanded','false');
+				//Remettre le focus au bouton qu'on avait cliqué pour ouvrir le volet
+				$('.last-focus').focus();
+				$('.last-focus').removeClass('last-focus');
+			});
+		}
+	}); //fin document ready
+})( jQuery );
